31f290ad7b588e350ab9387467d706452a1d855d
Wati-R <136698095+Wati-R@users.noreply.github.com>
2023-08-08 16:04:59 +0530

    Update linuxsysfw.sh

--- linuxsysfw.sh
+++ linuxsysfw.sh
@@ -34,68 +34,81 @@ then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /home/webadmin/apache-tomcat-8.0.36/bin/logs/geps-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'gg-celeste' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/gamv.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'webGOMS' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /root/tomcat/latest/bin/logs/goms-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'webapp01' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/gcart-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'webapp01' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/gcart-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'GGCT' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/courier-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 
 elif [ $var == 'ggehs' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/ehs-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 
 elif [ $var == 'GG-GQAP' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/gqap-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'ggmis' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/dwbi-dgr-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'gg-hisqlbl' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/goep-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'ggetdprod' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/etd-prod.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'ggdev02' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/appname.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}"
+  "${INSTALL_DIR}"/bin/splunk restart
 elif [ $var == 'ggdev01' ]
 then
   echo "Identifing Server"
   echo "Host Name detected"$str
   "${INSTALL_DIR}"/bin/splunk add monitor /logs/appname.log -index greenkolinux -auth "${ADMIN_USERNAME}":"${ADMIN_PASSWORD}" 
+  "${INSTALL_DIR}"/bin/splunk restart
 else
 echo "Unable to Find The Host"
 fi
